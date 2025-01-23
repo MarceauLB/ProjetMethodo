304ca574b0adcff959092ab2b774f2b42cf7eed5
Alexandre Maghames <alexandre.maghames@hotmail.com>
2025-01-23 17:15:12 +0100

    experients real-data

--- real-word-datasets/experiments/ar10p-exp.R
+++ real-word-datasets/experiments/ar10p-exp.R
@@ -1,5 +1,10 @@
 rm(list=ls())
-setwd("~/00_Ensai/projet-methodo/ProjetMethodo/real-word-datasets/dataset_csv/")
+setwd("~/00_Ensai/projet-methodo/ProjetMethodo/real-word-datasets/experiments/")
 
-ar10p <- read.csv("ar10p.csv")
-head(ar10p,10)
+ar10p <- read.csv("../dataset_csv/ar10p.csv")
+
+
+Y <- ar10p[,2401]
+X <- ar10p[,1:2400]
+table(Y)
+
